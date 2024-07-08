0e55814e15f726ee3176b46c1e1f0d2edf670b16
rohanray21 <128014880+rohanray21@users.noreply.github.com>
2024-07-08 05:15:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 11111
+let accountEmail = "ok@gmail.com"
+var accountPassword = "123456"
+accountCity = "Gkp"
+let accountState;
+
+// accountId = 2 (not allowed)
+
+accountEmail = "123@gmail.com"
+accountPassword = "23456"
+accountCity = "AAA"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState]);
