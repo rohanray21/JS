1d2f87ca2fe0a5e31536df7e17a846974854161f
rohanray21 <128014880+rohanray21@users.noreply.github.com>
2024-07-08 05:16:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat al JS code as newer version
+
+// alert("3-3") //we are using node.js not browser
+
+//number => 2 to the power 53
+//bigInt
+//string => ""
+//boolean = True/false
+//null => standalone value
+// undefiend =>
+// symbol => unique
+//object 
+
+console.log(typeof undefined);
+console.log(typeof null);
+
